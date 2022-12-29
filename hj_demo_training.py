020e5f219379d5a855392507e0d417f342e63d15
gemhou <1911061@tongji.edu.cn>
2022-12-29 18:56:58 +0800

    improve: prepare_dataset

--- hj_demo_training.py
+++ hj_demo_training.py
@@ -1,19 +1,17 @@
 from datasets import load_dataset
 from transformers import AutoTokenizer
+from transformers import AutoModelForSequenceClassification
 
 
-def prepare_dataset_1():
-    dataset = load_dataset("yelp_review_full")
-    print('dataset[train][100]: ', dataset["train"][100])
-    return dataset
-
-
-def prepare_dataset_2(dataset):
+def tokenize_function(examples):
     model_name = "bert-base-cased"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
+    return tokenizer(examples["text"], padding="max_length", truncation=True)
 
-    def tokenize_function(examples):
-        return tokenizer(examples["text"], padding="max_length", truncation=True)
+
+def prepare_dataset():
+    dataset = load_dataset("yelp_review_full")
+    print('dataset[train][100]: ', dataset["train"][100])
 
     tokenized_datasets = dataset.map(tokenize_function, batched=True)
     small_train_dataset = tokenized_datasets["train"].shuffle(seed=42).select(range(1000))
@@ -22,9 +20,7 @@ def prepare_dataset_2(dataset):
 
 
 def main():
-    dataset = prepare_dataset_1()
-
-    small_train_dataset, small_eval_dataset = prepare_dataset_2(dataset)
+    small_train_dataset, small_eval_dataset = prepare_dataset()
 
     print("finished...")
 
