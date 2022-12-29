b4b803f248d94f170cbb432745a2ff0b868c3c76
gemhou <1911061@tongji.edu.cn>
2022-12-29 15:53:13 +0800

    improve: I do not think it is not okay.

--- hj_demo_sentiment_analysis.py
+++ hj_demo_sentiment_analysis.py
@@ -4,8 +4,18 @@ from transformers import pipeline
 def main():
     print("hello world...")
     classifier = pipeline('sentiment-analysis')
-    returns = classifier('We are very glad to introduce pipeline to the transformers repository.')
-    print("returns: ", returns)
+    results = classifier('We are very glad to introduce pipeline to the transformers repository.')[0]
+    print("results: ", results)
+    results = classifier('I hate you.')[0]
+    print("results: ", results)
+    results = classifier('I love you.')[0]
+    print("results: ", results)
+    results = classifier('Haha.')[0]
+    print("results: ", results)
+    results = classifier('I do not think it is not okay.')[0]
+    print("results: ", results)
+    results = classifier('I do think it is okay.')[0]
+    print("results: ", results)
 
 
 if __name__ == '__main__':
