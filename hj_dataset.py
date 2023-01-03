b49ceee1ab0fc42881c0a02393219666436e540f
gemhou <1911061@tongji.edu.cn>
2023-01-03 17:29:47 +0800

    improve: dataset tokenize

--- hj_dataset.py
+++ hj_dataset.py
@@ -25,7 +25,9 @@ def tokenize_function(examples):
 def prepare_dataset():
     # yelp
     dataset_yelp = load_dataset("yelp_review_full")
-    # tokenized_datasets = dataset.map(tokenize_function, batched=True)
+    dataset_yelp["train"] = dataset_yelp["train"].select(range(1000))
+    dataset_yelp["test"] = dataset_yelp["test"].select(range(1000))
+    dataset_yelp = dataset_yelp.map(tokenize_function, batched=True)
     dataset_yelp = dataset_yelp.remove_columns(["text"])
     dataset_yelp = dataset_yelp.rename_column("label", "labels")
     dataset_yelp.set_format("torch")
@@ -36,12 +38,14 @@ def prepare_dataset():
 
     # wikipedia
     dataset_wikipedia = load_dataset("wikipedia", '20220301.simple', beam_runner='DirectRunner')
+    dataset_wikipedia["train"] = dataset_wikipedia["train"].select(range(1000))
+    dataset_wikipedia = dataset_wikipedia.map(tokenize_function, batched=True)
     dataset_wikipedia.set_format("torch")
     dataset_wikipedia = dataset_wikipedia["train"].shuffle(seed=42)  # .select(range(1000))
     dataloader_wikipedia  = DataLoader(dataset_wikipedia, shuffle=True, batch_size=6)
 
     # bookcorpus
-    dataset_bookcorpus = load_dataset("bookcorpus")
+    # dataset_bookcorpus = load_dataset("bookcorpus")
 
     return [dataset_yelp_train, dataset_yelp_test, dataloader_yelp_train, dataloader_yelp_eval], \
            [dataset_wikipedia, dataloader_wikipedia]
@@ -50,6 +54,9 @@ def prepare_dataset():
 def main():
     [dataset_yelp_train, dataset_yelp_test, dataloader_yelp_train, dataloader_yelp_eval], \
         [dataset_wikipedia, dataloader_wikipedia] = prepare_dataset()
+
+    batch_yelp_train = next(iter(dataloader_yelp_train))
+    batch_wikipedia = next(iter(dataloader_wikipedia))
 
     print("finish")
 
